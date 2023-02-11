adbd116d3028e40afb29784490d7d2d15090d9b6
yonatanAiz <67870348+yonatanAiz@users.noreply.github.com>
2023-02-11 16:58:37 +0200

    feat(button): make button forward ref (#7)
    * feat(button): make button forward ref
    
    * remove pack
    
    ---------
    
    Co-authored-by: yonatanAiz <yonatan.aiz@herolo.co.il>

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,30 +9,38 @@ export interface ButtonProps
   variant?: 'contained' | 'outlined' | 'text';
 }
 
-const Button = ({
-  color = 'primary',
-  variant = 'contained',
-  type = 'button',
-  icon,
-  children,
-  className,
-  ...props
-}: ButtonProps) => {
-  return (
-    <button
-      {...props}
-      type={type}
-      className={classnames(
-        styles.button,
-        className,
-        styles[variant],
-        styles[color],
-      )}
-    >
-      {icon ? <span data-testid="button-icon-prefix">{icon}</span> : null}
-      {children}
-    </button>
-  );
-};
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      color = 'primary',
+      variant = 'contained',
+      type = 'button',
+      icon,
+      children,
+      className,
+      ...props
+    },
+    ref,
+  ) => {
+    return (
+      <button
+        {...props}
+        ref={ref}
+        type={type}
+        className={classnames(
+          styles.button,
+          className,
+          styles[variant],
+          styles[color],
+        )}
+      >
+        {icon ? <span data-testid="button-icon-prefix">{icon}</span> : null}
+        {children}
+      </button>
+    );
+  },
+);
+
+Button.displayName = 'Button';
 
 export default Button;
